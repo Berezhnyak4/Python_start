ea96185b06d09defeb373a4cc1fcef0a79f78f9d
[Berezhnyak] <[deniska13_13@bk.ru]>
2020-11-22 16:53:27 +0300

    All Complite

--- HW_6.py
+++ HW_6.py
@@ -8,32 +8,26 @@
 соответствующее сообщение и завершать скрипт."""
 #OpenDataScience
 
-# from time import sleep
-#
-#
-# class TraffikLight:
-#     __color=["Красный", "Желтый", "Зеленый"]
-#     __time=[7,2,3] #добавил второй атрибут чтобы не указывать в ручную время
-#     def runnind(self):
-#         i=0
-#         while i < 3:
-#             print(f'Горит {TraffikLight.__color[i]}, {TraffikLight.__time[i]} секунд')
-#             if i == 0:
-#                 sleep(TraffikLight.__time[i])
-#             elif i == 1:
-#                 sleep(TraffikLight.__time[i])
-#             elif i == 2:
-#                 sleep(TraffikLight.__time[i])
-#                 i-=3
-#             i+=1
-# TraffikLight=TraffikLight()
-# TraffikLight.runnind()
+from time import sleep
 
 
-
-
-
-
+class TraffikLight:
+    __color=["Красный", "Желтый", "Зеленый"]
+    __time=[7,2,3] #добавил второй атрибут чтобы не указывать в ручную время
+    def runnind(self):
+        i=0
+        while i < 3:
+            print(f'Горит {TraffikLight.__color[i]}, {TraffikLight.__time[i]} секунд')
+            if i == 0:
+                sleep(TraffikLight.__time[i])
+            elif i == 1:
+                sleep(TraffikLight.__time[i])
+            elif i == 2:
+                sleep(TraffikLight.__time[i])
+                i-=3
+            i+=1
+TraffikLight=TraffikLight()
+TraffikLight.runnind()
 
 
 """2. Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина).
@@ -42,21 +36,21 @@
  полотна. Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв метра дороги
   асфальтом, толщиной в 1 см * число см толщины полотна. Проверить работу метода.
 Например: 20м * 5000м * 25кг * 5см = 12500 т"""
-# class RoadtotheDreem():
-#     def __init__(self, leght, width):
-#         self.leght = leght
-#         self.wigth = width
-#
-#     def mass(self):
-#         return f'{self.leght}m * {self.wigth}m * {self.volume}кг * {self.cm}см = {self.leght * self.wigth * self.volume * self.cm/1000} т'
-#
-# class MassCount(RoadtotheDreem):
-#     def __init__(self, _length, _width, volume, cm):
-#         super().__init__(_length, _width)
-#         self.volume = volume
-#         self.cm = cm
-# road = MassCount(20, 5000, 25, 5)
-# print(road.mass())
+class RoadtotheDreem():
+    def __init__(self, leght, width):
+        self.leght = leght
+        self.wigth = width
+
+    def mass(self):
+        return f'{self.leght}m * {self.wigth}m * {self.volume}кг * {self.cm}см = {self.leght * self.wigth * self.volume * self.cm/1000} т'
+
+class MassCount(RoadtotheDreem):
+    def __init__(self, _length, _width, volume, cm):
+        super().__init__(_length, _width)
+        self.volume = volume
+        self.cm = cm
+road = MassCount(20, 5000, 25, 5)
+print(road.mass())
 
 """3. Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname,
  position (должность), income (доход). Последний атрибут должен быть защищенным и ссылаться на 
@@ -65,24 +59,24 @@
   полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income). Проверить 
   работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить
    значения атрибутов, вызвать методы экземпляров)."""
-# class Worker():
-#     pro_dict={"wage": 500, "bonus": 100}
-#     def __init__(self, name, surname, position, wage, bonus):
-#         self.name = name
-#         self.surname = surname
-#         self.position = position
-#         self._income = {"wage": wage, "bonus": bonus}
-# class Position(Worker):
-#     def __init__(self, name, surname, position, wage, bonus):
-#         super().__init__(name, surname, position, wage, bonus)
-#     def get_full_name(self):
-#         return f'Full name: {self.name} {self.surname}'
-#     def get_total_income(self):
-#         return f'Profit: {sum(self._income.values())}'
-# I = Position("Igor", "Spchkin", "pro", 500, 200)
-# print(I.position)
-# print(I.get_full_name())
-# print(I.get_total_income())
+class Worker():
+    pro_dict={"wage": 500, "bonus": 100}
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+    def get_full_name(self):
+        return f'Full name: {self.name} {self.surname}'
+    def get_total_income(self):
+        return f'Profit: {sum(self._income.values())}'
+I = Position("Igor", "Spchkin", "pro", 500, 200)
+print(I.position)
+print(I.get_full_name())
+print(I.get_total_income())
 """4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color,
  name, is_police (булево). А также методы: go, stop, turn(direction), которые должны сообщать, что
   машина поехала, остановилась, повернула (куда). Опишите несколько дочерних классов: TownCar, 
@@ -137,17 +131,17 @@ class SportCar(Car):
         super().__init__(speed, color, name, is_police)
     def show_speed(self):
         if self.speed > 110:
-            return f'Скорость выше 110 км/ч'
+            return f'Скорость выше 110 км/ч, притормози!'
         else:
             return f'Машина {self.name} едет со скоростью {self.speed} км/ч'
-meren = SportCar(140, 'Белый', 'Новый мерен', False)
-oka = TownCar(30, 'Белая', 'ока', False)
-lada = WorkCar(50, 'Баклажан', 'лада-седан', True)
+meren = SportCar(140, 'белый', 'новый мерен', False)
+oka = TownCar(35, 'черная', 'ока', False)
+lada = WorkCar(50, 'баклажан', 'лада-седан', True)
 ford = PoliceCar(110, 'ментовской',  'форд', True)
 print(lada.turn_l())
 print(f'Когда {oka.turn_r()}, тогда {meren.stop()}')
 print(f'{lada.go()}')
-print(f'{lada.name} это {lada.color}')
+print(f'{lada.name} цвета {lada.color}')
 print(f' {meren.name} полицейская тачка? {meren.is_police}')
 print(f'А {lada.name}  полицейская тачка? {lada.is_police}')
 print(meren.show_speed())
@@ -160,3 +154,33 @@ print(ford.show_speed())
   классов реализовать переопределение метода draw. Для каждого из классов методы должен 
   выводить уникальное сообщение. Создать экземпляры классов и проверить, что выведет описанный
    метод для каждого экземпляра."""
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+class Pensil(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+    def draw(self):
+        return f'Делаем контур и тени'
+class Handle(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+    def draw(self):
+        return f'Делаем фон'
+class Pen(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+    def draw(self):
+        return f'Росчерк пера'
+begin = Stationery('Шедевр')
+pen = Pen("Подпись")
+fon = Handle("Фон")
+black = Pensil("Рисуем контуры")
+print(begin.draw())
+print(fon.draw())
+print(black.draw())
+print(pen.draw())
+
+
